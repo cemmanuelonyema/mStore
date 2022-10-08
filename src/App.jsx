9dc2ae5b2311f1e16705d09f8db213841e608143
cemmanuelonyema <cemmanuelonyema@gmail.com>
2022-10-08 16:26:25 +0100

    update: add 3 pages - home, shop, notFound

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import React, { Fragment } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Layout } from './layout';
+//pages
+import { Home } from './pages/home/Home';
+import { NotFound } from './pages/notFound/NotFound';
+import { Shop } from './pages/shop/Shop';
 
 export const App = () => {
   return (
     <Fragment>
       <Routes>
-        <Route path="/" element={<Layout />}></Route>
+        <Route path="/" element={<Layout />}>
+          <Route index path="/" element={<Home />} />
+          <Route index path="shop" element={<Shop />} />
+
+          <Route index path="*" element={<NotFound />} />
+        </Route>
       </Routes>
     </Fragment>
   );
