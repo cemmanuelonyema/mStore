9b8d0d0e7fa5a33eef62f6760d2a3caab3c63552
cemmanuelonyema <cemmanuelonyema@gmail.com>
2022-10-08 16:42:47 +0100

    style: add styled global style setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { Fragment } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Layout } from './layout';
+import { GlobalStyle } from './style/GlobalStyle';
+
 //pages
 import { Home } from './pages/home/Home';
 import { NotFound } from './pages/notFound/NotFound';
@@ -9,6 +11,7 @@ import { Shop } from './pages/shop/Shop';
 export const App = () => {
   return (
     <Fragment>
+      <GlobalStyle />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index path="/" element={<Home />} />
