871bb47245bea0929aae10eb2d6d92eb2dad6952
cemmanuelonyema <cemmanuelonyema@gmail.com>
2022-11-14 08:26:21 +0100

    update: add hero content content and images

--- src/components/sections/home/hero/Hero.jsx
+++ src/components/sections/home/hero/Hero.jsx
@@ -4,42 +4,48 @@ import { Content, Images, StyledHero } from './StyledHero';
 export const Hero = () => {
   return (
     <StyledHero>
-      <div className="container">
-        <Content>
-          <h1>
-            <div className="line">
-              <p>Relieveing the burden</p>
+      <div className="hero__container">
+        <div className="hero__inner">
+          <Content>
+            <div className="content__inner">
+              <h1>
+                <div className="line">
+                  <span>Relieveing the burden of</span>
+                </div>
+                <div className="line">
+                  <span>lack of premium wears </span>
+                </div>
+                <div className="line">
+                  <span>for men</span>
+                </div>
+              </h1>
+              <p>
+                Lorem jhskhdbjsn nsn hsmn jjs jj Lorem ipsum dolor sit, amet
+                consectetur adipisicing elit. Minus, doloremque!
+              </p>
+              <button>
+                Explore
+                <span> ➡️ </span>
+              </button>
             </div>
-            <div className="line">
-              <p>of disease caused </p>
+          </Content>
+          <Images>
+            <div className="images__inner">
+              <div className="image-box image-box--1">
+                <img
+                  src="/public/img/suits/janko-ferlic-G-jo31ESuRE-unsplash.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="image-box  image-box--2">
+                <img
+                  src="/public/img/suits/khalid-boutchich-wS1k0VzXVrA-unsplash.jpg"
+                  alt=""
+                />
+              </div>
             </div>
-            <div className="line">
-              <p>by behavior</p>
-            </div>
-          </h1>
-          <p>
-            Lorem jhskhdbjsn nsn hsmn jjs jj Lorem ipsum dolor sit, amet
-            consectetur adipisicing elit. Minus, doloremque!
-          </p>
-          <button>
-            Explore
-            <span> ➡️ </span>
-          </button>
-        </Content>
-        <Images>
-          <div className="">
-            <img
-              src="/public/img/suits/janko-ferlic-G-jo31ESuRE-unsplash.jpg"
-              alt=""
-            />
-          </div>
-          <div className="">
-            <img
-              src="/public/img/suits/khalid-boutchich-wS1k0VzXVrA-unsplash.jpg"
-              alt=""
-            />
-          </div>
-        </Images>
+          </Images>
+        </div>
       </div>
     </StyledHero>
   );
--- src/components/sections/home/hero/StyledHero.jsx
+++ src/components/sections/home/hero/StyledHero.jsx
@@ -10,8 +10,80 @@ export const StyledHero = styled.section`
     margin: 0 auto;
     padding: 3rem;
   }
+
+  .hero__inner {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 0 24px;
+    height: 100vh;
+  }
 `;
 
+export const Content = styled.div`
+  width: 50%;
+  .content__inner {
+    width: 400px;
+    margin: 0 auto;
 
-export const Content = styled.div``
-export const Images = styled.div``
+    h1 {
+      font-weight: bold;
+      font-size: 3.2rem;
+      margin-bottom: 2.4rem;
+    }
+
+    .line {
+      height: 45px;
+      overflow: hidden;
+    }
+
+    p {
+      font-weight: 300;
+      line-height: 24px;
+      font-size: 14px;
+      margin-bottom: 50px;
+    }
+
+    button {
+      background: none;
+      cursor: pointer;
+      text-transform: uppercase;
+      font-weight: bold;
+      display: flex;
+      align-items: center;
+      gap: 1rem;
+      position: relative;
+      padding: 1rem;
+
+      &:focus {
+        outline: none;
+      }
+    }
+  }
+`;
+export const Images = styled.div`
+  width: 50%;
+  height: 100vh;
+  position: relative;
+  .images__inner {
+  }
+
+  .image-box {
+    overflow: hidden;
+    position: absolute;
+
+    &--1 {
+      top: 0;
+      right: 0;
+      width: 45%;
+      height: 50%;
+    }
+
+    &--2 {
+      bottom: 24px;
+      left: 0px;
+      width: 52%;
+      height: 60%;
+    }
+  }
+`;
