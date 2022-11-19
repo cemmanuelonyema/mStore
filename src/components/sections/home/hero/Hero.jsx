2fbd3ce537892b2c94106fcefc1304f1b25f5f4c
cemmanuelonyema <cemmanuelonyema@gmail.com>
2022-11-19 14:19:17 +0100

    modify hero content

--- src/components/sections/home/hero/Hero.jsx
+++ src/components/sections/home/hero/Hero.jsx
@@ -92,47 +92,45 @@ export const Hero = () => {
   return (
     <StyledHero ref={(el) => (app = el)}>
       <div className="hero__container">
-        <div className="hero__inner">
-          <Content>
-            <div className="content__inner" ref={(el) => (content = el)}>
-              <h1>
-                <div className="line">
-                  <span>Relieveing the burden of</span>
-                </div>
-                <div className="line">
-                  <span>lack of premium wears </span>
-                </div>
-                <div className="line">
-                  <span>for men</span>
-                </div>
-              </h1>
-              <p>
-                Lorem jhskhdbjsn nsn hsmn jjs jj Lorem ipsum dolor sit, amet
-                consectetur adipisicing elit. Minus, doloremque!
-              </p>
-              <button>
-                Explore
-                <span> ➡️ </span>
-              </button>
-            </div>
-          </Content>
-          <Images>
-            <div className="images__inner" ref={(el) => (images = el)}>
-              <div className="image-box image-box--1">
-                <img
-                  src="/public/img/suits/janko-ferlic-G-jo31ESuRE-unsplash.jpg"
-                  alt=""
-                />
+        <Content>
+          <div className="content__inner" ref={(el) => (content = el)}>
+            <h1>
+              <div className="line">
+                <span>Relieveing the burden of</span>
               </div>
-              <div className="image-box  image-box--2">
-                <img
-                  src="/public/img/suits/khalid-boutchich-wS1k0VzXVrA-unsplash.jpg"
-                  alt=""
-                />
+              <div className="line">
+                <span>lack of premium wears </span>
               </div>
+              <div className="line">
+                <span>for men</span>
+              </div>
+            </h1>
+            <p>
+              Lorem jhskhdbjsn nsn hsmn jjs jj Lorem ipsum dolor sit, amet
+              consectetur adipisicing elit. Minus, doloremque!
+            </p>
+            <button>
+              Explore
+              <span> ➡️ </span>
+            </button>
+          </div>
+        </Content>
+        <Images>
+          <div className="images__inner" ref={(el) => (images = el)}>
+            <div className="image-box image-box--1">
+              <img
+                src="/public/img/suits/janko-ferlic-G-jo31ESuRE-unsplash.jpg"
+                alt=""
+              />
             </div>
-          </Images>
-        </div>
+            <div className="image-box  image-box--2">
+              <img
+                src="/public/img/suits/khalid-boutchich-wS1k0VzXVrA-unsplash.jpg"
+                alt=""
+              />
+            </div>
+          </div>
+        </Images>
       </div>
     </StyledHero>
   );
--- src/components/sections/home/hero/StyledHero.jsx
+++ src/components/sections/home/hero/StyledHero.jsx
@@ -2,35 +2,42 @@ import styled from 'styled-components';
 
 export const StyledHero = styled.section`
   border: 1px solid black;
-  margin-top: 10rem;
-  /* padding-top: 25rem; */
+  margin-top: 7rem;
   visibility: hidden;
 
-  .container {
+  .hero__container {
     max-width: 120rem;
+    min-height: 70vh;
+    /* height: 100vh; */
     margin: 0 auto;
     padding: 3rem;
-  }
 
-  .hero__inner {
     display: flex;
-    align-items: center;
     justify-content: space-between;
-    margin: 0 24px;
-    /* height: 100vh; */
+    align-items: center;
+    gap: 6.4rem;
+
+    @media screen and (max-width: 37.5em) {
+      flex-direction: column;
+    }
   }
 `;
 
 export const Content = styled.div`
   width: 50%;
+  @media screen and (max-width: 37.5em) {
+    width: 100%;
+  }
   .content__inner {
-    width: 400px;
+    max-width: 400px;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
     margin: 0 auto;
 
     h1 {
       font-weight: bold;
       font-size: 3.2rem;
-      margin-bottom: 2.4rem;
     }
 
     .line {
@@ -45,7 +52,6 @@ export const Content = styled.div`
       font-weight: 300;
       line-height: 24px;
       font-size: 14px;
-      margin-bottom: 50px;
     }
 
     button {
@@ -69,6 +75,10 @@ export const Images = styled.div`
   width: 50%;
   height: 100vh;
   position: relative;
+
+  @media screen and (max-width: 37.5em) {
+    width: 100%;
+  }
   .images__inner {
   }
 
@@ -88,6 +98,9 @@ export const Images = styled.div`
       left: 0px;
       width: 52%;
       height: 60%;
+      @media screen and (max-width: 37.5em) {
+        bottom: 20rem;
+      }
     }
     img {
       position: absolute;
